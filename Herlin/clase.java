d7742bea000563408dba3ba17e57fd83821c6d9b
RobertoJCastillo <roberto_16051995@hotmail.com>
2023-03-15 17:58:09 -0600

    Creacion del banco y objetos cuenta

--- Herlin/clase.java
+++ Herlin/clase.java
@@ -1,4 +1,18 @@
 package Herlin;
 
 public class clase {
+    private Double cuentaDolares;
+
+    public clase(){
+        cuentaDolares=0.0;
+    }
+
+    public Double retirar(Double monto) {
+        if (monto >= cuentaDolares) {
+            cuentaDolares = cuentaDolares - monto;
+            return monto;
+        } else {
+            return null;
+        }
+    }
 }
